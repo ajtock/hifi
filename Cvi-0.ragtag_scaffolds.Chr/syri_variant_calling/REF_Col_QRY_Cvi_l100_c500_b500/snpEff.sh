a5f218eccf69bf4541c3df93d728909647339680
ajtock <andytock@gmail.com>
2022-01-12 17:56:49 +0000

    REF_Col_QRY_Cvi_l100_c500_b500

---
+++ Cvi-0.ragtag_scaffolds.Chr/syri_variant_calling/REF_Col_QRY_Cvi_l100_c500_b500/snpEff.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run snpEff v5.0e on SyRi v1.5 output VCF file
+
+# For details of the file format, please check https://schneebergerlab.github.io/syri/fileformat.html
+
+# Usage:
+# csmit -m 100G -c 1 "bash ./snpEff.sh t2t-col.20210610.Chr Cvi-0.ragtag_scaffolds.Chr"
+
+REF=$1
+QRY=$2
+
+#source activate syri
+#
+#echo $(which snpEff)
+
+java -Xmx100G -jar /home/ajt200/miniconda3/envs/syri/share/snpeff-5.0-1/snpEff.jar -verbose \
+              -config /home/ajt200/miniconda3/envs/syri/share/snpeff-5.0-1/snpEff.config ${REF} ${REF}_${QRY}_nm3_syri_filt.vcf \
+              > ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf
+
+#source deactivate
---
+++ Cvi-0.ragtag_scaffolds.Chr/syri_variant_calling/REF_Col_QRY_Cvi_l100_c500_b500/snpEff_grep_impact.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Run snpEff v5.0e on SyRi v1.5 output VCF file
+
+# For details of the file format, please check https://schneebergerlab.github.io/syri/fileformat.html
+
+# Usage:
+# bash ./snpEff_grep_impact.sh t2t-col.20210610.Chr Cvi-0.ragtag_scaffolds.Chr
+
+REF=$1
+QRY=$2
+
+head -n 36 ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf > ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf
+
+grep "HIGH" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_HIGH.vcf
+grep "MODERATE" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_MODERATE.vcf
+grep "LOW" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_LOW.vcf
+grep "MODIFIER" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_MODIFIER.vcf
+grep "LOF" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | grep "HIGH" - | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_LOF_AND_HIGH.vcf
+grep "NMD" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | grep "HIGH" - | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_NMD_AND_HIGH.vcf
+grep "LOF" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | grep "NMD" - | grep "HIGH" - | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_LOF_NMD_AND_HIGH.vcf
+grep "LOF" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | grep -v "HIGH" - | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_LOF_NOT_HIGH.vcf
+grep "NMD" ${REF}_${QRY}_nm3_syri_filt.snpEff.vcf | grep -v "HIGH" - | cat ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf - > ${REF}_${QRY}_nm3_syri_filt.snpEff_NMD_NOT_HIGH.vcf
+
+rm ${REF}_${QRY}_nm3_syri_filt.snpEff_HEADER.vcf
