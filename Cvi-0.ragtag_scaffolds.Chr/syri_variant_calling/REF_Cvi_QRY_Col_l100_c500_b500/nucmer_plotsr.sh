3262a310508b7c838b0aef5f06a297e37266bddc
ajtock <andytock@gmail.com>
2022-01-12 17:57:08 +0000

    REF_Cvi_QRY_Col_l100_c500_b500

---
+++ Cvi-0.ragtag_scaffolds.Chr/syri_variant_calling/REF_Cvi_QRY_Col_l100_c500_b500/nucmer_plotsr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Generate graphical representation of genomic structure using output of SyRI variant calling on nucmer-aligned assemblies
+# SyRI v1.5 (https://github.com/schneebergerlab/syri).
+
+# For details of the file format, please check https://schneebergerlab.github.io/syri/plotsr.html
+# Usage:
+# bash ./nucmer_plotsr.sh Cvi-0.ragtag_scaffolds.Chr t2t-col.20210610.Chr
+
+REF=$1
+QRY=$2
+
+source activate syri
+
+echo $(which nucmer)
+echo $(nucmer --version)
+echo $(which plotsr)
+
+/home/ajt200/envs_miniconda3/syri_1.5/syri/bin/plotsr ${REF}_${QRY}_nm3_syri.out \
+                                                      ${REF}.fa ${QRY}.fa
+
+mv syri.pdf ${REF}_${QRY}_nm3_syri_plotsr.pdf
+
+conda deactivate
+
+
